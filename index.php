e814fab31e25b4e8b7398dee377f8c56c9da4dfa
Gene Stein <gene.stein@me.com>
2018-03-11 20:11:28 +0000

    this should do it

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 </head>
 <body>
 	Hello World!
-
-
+<form>
+<input type="button" value="to bidPage" onclick="window.location.href='bidPage.php'" />
+<input type="button" value="to buyerHome" onclick="window.location.href='buyerHome.php'" />
+<input type="button" value="to buyerSearch-form" onclick="window.location.href='buyerSearch-form.php'" />
+<input type="button" value="to placeBid" onclick="window.location.href='placeBid.php'" />
+</form>
 </body>
 </html>
