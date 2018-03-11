c241a860e6cff4e2c3a3c412dd7130ce5ba52ddb
Gene Stein <gene.stein@me.com>
2018-03-11 20:05:00 +0000

    added buttons to index

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Hello World!";
+<form>
+<input type="button" value="to bidPage" onclick="window.location.href='bidPage.php'" />
+<input type="button" value="to buyerHome" onclick="window.location.href='buyerHome.php'" />
+<input type="button" value="to buyerSearch-form" onclick="window.location.href='buyerSearch-form.php'" />
+<input type="button" value="to placeBid" onclick="window.location.href='placeBid.php'" />
+</form>
 
 ?>
