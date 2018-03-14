64eb565f53b942175e8e4856dcf8fccee0fea78c
Gene Stein <gene.stein@me.com>
2018-03-14 22:19:29 +0000

    huh

--- newItem.php
+++ newItem.php
@@ -27,7 +27,7 @@ if($endDate > $date) {
 
 	if ($db -> query ($sql) === TRUE ) {
 		echo "New Record Successfully Created";
-		echo "<a href="sellerHome.php">Return to Home</a>";
+		echo "<a href='sellerHome.php'>Return to Home</a>";
 	} else {
  echo "error: " .$sql. "<br> " . $db -> error;
  		}
