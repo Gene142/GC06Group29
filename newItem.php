eff7ab70763673eff49026aecc738288345a88af
Gene Stein <gene.stein@me.com>
2018-03-14 22:09:29 +0000

    trying to get add item perfect

--- newItem.php
+++ newItem.php
@@ -12,7 +12,7 @@ $endDate = $_POST ["endDate"];
 $name = $_POST ["name"];
 //$highestBidID = $_POST ["highestBidID"];
 
-
+$date = date('Y-m-d H:i:s');
 
 
 
@@ -20,17 +20,16 @@ $db = new mysqli('dbauction.mysql.database.azure.com', 'group29admin@dbauction',
 or  die('Could not connect: ');
 	
 //first double check endDate is > now
-if($endDate > CURRENT_TIMESTAMP()){
-$sql = "INSERT INTO items (name, description, startPrice, resPrice, categoryId, endDate) 
-VALUES('$name','$description','$startPrice', '$resPrice', '$categoryId', '$endDate');";
+if($endDate > $date) {
 
+	$sql = "INSERT INTO items (name, description, startPrice, resPrice, categoryId, endDate) 
+	VALUES('$name','$description','$startPrice', '$resPrice', '$categoryId', '$endDate');";
 
-if ($db -> query ($sql) === TRUE )
-{
-echo "New Record Successfully Created" ;
-} else {
+	if ($db -> query ($sql) === TRUE ) {
+		echo "New Record Successfully Created" ;
+	} else {
  echo "error: " .$sql. "<br> " . $db -> error;
- }
+ 		}
 }
 
  $db -> close ();
