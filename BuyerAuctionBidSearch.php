d2d813a788996425ca01b634263d7ee0e20df835
Gene Stein <gene.stein@me.com>
2018-03-14 20:59:08 +0000

    trying to add sorts

--- BuyerAuctionBidSearch.php
+++ BuyerAuctionBidSearch.php
@@ -1,13 +1,23 @@
+<?php SESSION_START(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action = "BuyerAuctionBidSearch.php" method = "post">
+	<input type="submit" value="A.bidAmount" name = 'sortOption' />
+    <input type="submit" value="B.name" name = 'sortOption' />
+    <input type="submit" value="B.endDate" name = 'sortOption' />
+</form>
 <?php
-SESSION_START();
 
 $categoryId = $_POST['categoryId'];
-
+$sortOption = $_POST['sortOption']
 
 $db = mysqli_connect('dbauction.mysql.database.azure.com', 'group29admin@dbauction', 'Ilovedatabases1', 'auction')
 or  die('Could not connect: ');
 
-$sql = "SELECT B.itemId, B.name, B.description, B.categoryId, A.bidAmount FROM (select itemId, max(bidAmount) AS bidAmount from bids group by itemId) as A RIGHT OUTER JOIN (SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId') AS B ON A.itemId = B.itemId;";
+$sql = "SELECT B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate FROM (select itemId, max(bidAmount) AS bidAmount from bids group by itemId) as A RIGHT OUTER JOIN (SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId') AS B ON A.itemId = B.itemId ORDER BY $sortOption;";
 		
 $result = $db->query($sql)
 or die('Error with query'); 
@@ -21,8 +31,9 @@ while ($row = $result->fetch_assoc()) {
 echo '<tr><td>'."Name". '</td>'. '<td>' . $row['name'].'</td></tr>';
 echo '<tr><td>' ."Item Description". '</td>'. '<td>' . $row['description'].'</td></tr>';
 echo '<tr><td>' . "Bid Amount". '</td>'. '<td>' .'£'. $row['bidAmount'].'</td></tr>';
+echo '<tr><td>' . "End Date". '</td>'. '<td>' .'£'. $row['endDate'].'</td></tr>';
 echo '<tr><td>'."<a href='bidPage.php?id=".$row['itemId']."'>BID ON ITEM</a>".'</td>'. '<td>'."".'</td></tr>';
-//include a more bidOn button at bottom of table.	
+
 }
 
 
@@ -30,9 +41,7 @@ echo '<tr><td>'."<a href='bidPage.php?id=".$row['itemId']."'>BID ON ITEM</a>".'<
 echo '</table>';
 
 mysqli_close($db);
-
-
-
-
  ?>
+</body>
+</html>
 
