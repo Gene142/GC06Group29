1a5be6442a3b8b6db26dd91248019bafab54a2e0
Gene Stein <gene.stein@me.com>
2018-03-14 18:03:11 +0000

    shs

--- placeBid.php
+++ placeBid.php
@@ -27,7 +27,7 @@ if ($highestBid == 0 || $highestBid == NULL) {
     }
 } else if($bidAmountEntered > $highestBid) {
 	//insert into database FIX BuyerID BIDAMOUNT entered below
-	$sql = "INSERT INTO bids (itemId, bidAmount, buyerId VALUES ('$itemIdReq','$bidAmountEntered', '$newBuyerId')";
+	$sql = "INSERT INTO bids (itemId, bidAmount, buyerId) VALUES ('$itemIdReq','$bidAmountEntered', '$newBuyerId')";
 	if ($db->query($sql) === TRUE) {
     	echo "Bid Successful";
     	include_once('sendMail.php');
