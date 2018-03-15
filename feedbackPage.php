40b50299e3f31bc7f5cc95a290fae7345b0aa0b6
Gene Stein <gene.stein@me.com>
2018-03-15 01:08:13 +0000

    trying feedback

--- feedbackPage.php
+++ feedbackPage.php
@@ -10,10 +10,9 @@ $buyerId = $_SESSION['buyerId'];
 $db = new mysqli('dbauction.mysql.database.azure.com', 'group29admin@dbauction', 'Ilovedatabases1', 'auction')
 or  die('Could not connect: ');
 	//no need for verification, feedback is unique for buyerId and sellerId
-	$sql = "INSERT INTO feedback (buyerId, sellerId, pointsGiven) VALUES ('$buyerId','$sellerId','pointsGiven'";
-	if ($db -> query ($sql) === TRUE ) {
-		echo "New Record Successfully Created";
-		echo "<a href='sellerHome.php'>Return to Home</a>";
+	$sql = "INSERT INTO feedback (buyerId, sellerId, pointsGiven) VALUES ('$buyerId','$sellerId','$pointsGiven'";
+	if ($db -> query($sql) === TRUE ) {
+		echo "Feedback Given";
 	} else {
  echo "error: " .$sql. "<br> " . $db -> error;
  		}
