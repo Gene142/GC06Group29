8be06d7213866306984a101578d0e9515c0309a2
Gene Stein <gene.stein@me.com>
2018-03-15 02:13:34 +0000

    trying feedback

--- BuyerAuctionBidSearch.php
+++ BuyerAuctionBidSearch.php
@@ -12,8 +12,8 @@ $sortOption = $_POST['sortOption'];
 $db = mysqli_connect('dbauction.mysql.database.azure.com', 'group29admin@dbauction', 'Ilovedatabases1', 'auction')
 or  die('Could not connect: ');
 
-$sql = "SELECT C.sellerId, C.itemId, C.name, C.description, C.categoryId, C.bidAmount, C.endDate, D.total FROM
-(SELECT B.sellerId, B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate FROM 
+$sql = "SELECT C.sellerId, C.itemId, C.name, C.description, C.categoryId, C.bidAmount, C.endDate, D.total, C.resPrice, C. FROM
+(SELECT B.sellerId, B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate, A.resPrice, FROM 
 (select itemId, max(bidAmount) AS bidAmount from bids group by itemId) as A RIGHT OUTER JOIN 
 (SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate, sellerId from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId') AS B ON A.itemId = B.itemId) AS C LEFT OUTER JOIN
  (SELECT SUM(pointsGiven) AS total, sellerId from feedback GROUP BY sellerId) AS D ON D.sellerId = C.sellerId ORDER BY $sortOption";
@@ -43,9 +43,10 @@ mysqli_close($db);
  ?>
 
  <form action = "BuyerAuctionBidSearch.php" method = "post">
-	<button type="submit" value="IFNULL(A.bidAmount, B.resPrice)" name = 'sortOption'>Sort By Price</button>
-    <button type="submit" value="B.name" name = 'sortOption'>Sort By Name</button>
-    <button type="submit" value="B.endDate" name = 'sortOption'>Sort By End Date</button>
+	<button type="submit" value="IFNULL(C.bidAmount, C.resPrice)" name = 'sortOption'>Sort By Price</button>
+    <button type="submit" value="C.name" name = 'sortOption'>Sort By Name</button>
+    <button type="submit" value="C.endDate" name = 'sortOption'>Sort By End Date</button>
+    <button type="submit" value="D.total" name = 'sortOption'>Sort By User Rating</button>
     <input type="hidden" name="categoryId" value="<?php echo $categoryId; ?>"/>
 </form>
 
