04ffd0b285c1b4034854ed498156a27f8b5b5e28
Gene Stein <gene.stein@me.com>
2018-03-15 02:03:02 +0000

    trying feedback

--- BuyerAuctionBidSearch.php
+++ BuyerAuctionBidSearch.php
@@ -13,10 +13,10 @@ $db = mysqli_connect('dbauction.mysql.database.azure.com', 'group29admin@dbaucti
 or  die('Could not connect: ');
 
 $sql = "SELECT * FROM
-(SELECT B.sellerId, B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate FROM 
+(SELECT B.sellerId, B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate, D.total FROM 
 (select itemId, max(bidAmount) AS bidAmount from bids group by itemId) as A RIGHT OUTER JOIN 
 (SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate, sellerId from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId') AS B ON A.itemId = B.itemId) AS C LEFT OUTER JOIN
- (SELECT SUM(pointsGiven), sellerId from feedback GROUP BY sellerId) AS D ON D.sellerId = C.sellerId"
+ (SELECT SUM(pointsGiven) as total, sellerId from feedback GROUP BY sellerId) AS D ON D.sellerId = C.sellerId";
 $result = $db->query($sql)
 or die('Error with query'); 
 echo '<tr>'."These Are the Current items on the category You have Searched For". '</tr>';
@@ -30,6 +30,7 @@ echo '<tr><td>'."Name". '</td>'. '<td>' . $row['name'].'</td></tr>';
 echo '<tr><td>' ."Item Description". '</td>'. '<td>' . $row['description'].'</td></tr>';
 echo '<tr><td>' . "Bid Amount". '</td>'. '<td>' .'£'. $row['bidAmount'].'</td></tr>';
 echo '<tr><td>' . "End Date". '</td>'. '<td>' .'£'. $row['endDate'].'</td></tr>';
+echo '<tr><td>' . "Seller Feedback Points". '</td>'. '<td>' .'£'. $row['total'].'</td></tr>';
 echo '<tr><td>'."<a href='bidPage.php?id=".$row['itemId']."'>BID ON ITEM</a>".'</td>'. '<td>'."".'</td></tr>';
 
 }
