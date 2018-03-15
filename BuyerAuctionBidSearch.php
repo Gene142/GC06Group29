f01d34e7e28b5923592077c09648b4d23ab3f5a0
Gene Stein <gene.stein@me.com>
2018-03-15 17:29:22 +0000

    adw

--- BuyerAuctionBidSearch.php
+++ BuyerAuctionBidSearch.php
@@ -8,14 +8,14 @@
 
 $categoryId = $_POST['categoryId'];
 $sortOption = $_POST['sortOption'];
-
+$description = $_POST['description'];
 $db = mysqli_connect('dbauction.mysql.database.azure.com', 'group29admin@dbauction', 'Ilovedatabases1', 'auction')
 or  die('Could not connect: ');
 
 $sql = "SELECT C.sellerId, C.itemId, C.name, C.description, C.categoryId, C.bidAmount, C.endDate, D.total, C.resPrice FROM
 (SELECT B.sellerId, B.itemId, B.name, B.description, B.categoryId, A.bidAmount, B.endDate, B.resPrice FROM 
 (select itemId, max(bidAmount) AS bidAmount from bids group by itemId) as A RIGHT OUTER JOIN 
-(SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate, sellerId from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId') AS B ON A.itemId = B.itemId) AS C LEFT OUTER JOIN
+(SELECT itemId, name, description, startPrice, resPrice, categoryId, endDate, sellerId from items WHERE endDate > CURRENT_TIMESTAMP() AND categoryId = '$categoryId' AND description LIKE '%$description%') AS B ON A.itemId = B.itemId) AS C LEFT OUTER JOIN
  (SELECT SUM(pointsGiven) AS total, sellerId from feedback GROUP BY sellerId) AS D ON D.sellerId = C.sellerId ORDER BY $sortOption ;";
 $result = $db->query($sql)
 or die('Error with query'); 
